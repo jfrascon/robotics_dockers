303a721ad6f76489b80c01b9447a65fea68f121d
jfrascon <jfrascon@gmail.com>
2026-06-12 00:13:11 +0200

    refactor(builder): [1206-05] rename ros2build.sh → ros2build, install as rosbuild

--- builder/create_docker_files.py
+++ builder/create_docker_files.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
             ".resources/install_base_system.sh": ["install_base_system.sh", True],
             ".resources/install_extra_pkgs.sh": ["install_extra_pkgs.sh", True],
             ".resources/install_ros.sh": ["install_ros2.sh", True],
-            ".resources/rosbuild.sh": ["ros2build.sh", True],
+            ".resources/rosbuild": ["ros2build", True],
             ".resources/rosdep_init_update_install.sh": ["rosdep_init_update_install.sh", True],
             # extra.d/ templates
             ".resources/extra.d/apt_packages.sh": ["extra.d/apt_packages.sh", True],
