858c1f9b9a9fdce22ada1f478c328514d96d0903
jfrascon <jfrascon@gmail.com>
2026-08-13 14:18:34 +0000

    build: install uv for development tools
    Add uv to the base user Python tool set so generated Docker images provide uv and uvx from ~/.local/bin. This keeps Python CLI tooling isolated while making MCP and other user-level tool installs easier inside the container.

--- src/robotics_dockers/resources/install_base_system.sh
+++ src/robotics_dockers/resources/install_base_system.sh
@@ -334,7 +334,7 @@ fi
 #---------------------------------------------------------------------------------------------------
 # Install Python packages for the user that are commonly used for development.
 #---------------------------------------------------------------------------------------------------
-python_packages=(argcomplete ruff cmake-format pre-commit jinja2 python-rapidjson)
+python_packages=(argcomplete ruff cmake-format pre-commit jinja2 python-rapidjson uv)
 
 log info "Installing Python packages for the user '${TARGET_USER}': ${python_packages[*]}"
 
